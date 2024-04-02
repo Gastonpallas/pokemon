63521ec7efa7a5d610841fd5eaf87e129a5333b1
gaston <pallasgaston@gmail.com>
2024-04-02 16:13:28 +0200

    Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {Pokemon} from "./pokemon";
-import {POKEMONS} from "./mock-pokemon-list";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent implements OnInit {
-  pokemons: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-
-  ngOnInit(): void {
-  }
-
-  selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon | undefined = this.pokemons.find(pokemon => pokemon.id == +pokemonId);
-    if (pokemon) {
-      console.log(`Vous avez choisi ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez choisi un pokemon qui n'existe pas`);
-      this.pokemonSelected = undefined;
-    }
-  }
-}
+export class AppComponent {}
